a9fcb7ea7cf4be868fc44ae955d35334ca791234
E Ek <eek.datasc@gmail.com>
2025-12-11 08:30:08 +0000

    1. Two Sum

---
+++ problems/1. Two Sum/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def twoSum(nums : List[int], target : int) -> list[int]:
+
+    seen = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num 
+
+        if complement in seen:
+            return [seen[complement], i]
+        
+        seen[num] = i
+
+
+
+
+print(twoSum([2,7,11,15], 9))  # Output: [0, 1]
+print(twoSum([3,2,4], 6))      # Output: [1, 2]
+print(twoSum([3,3], 6))        # Output: [0, 1]
